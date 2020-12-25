a7844f194c31fc46fd2b1703f97c0beae50e1331
Troky <lorenzorosinitroky@live.it>
2020-12-25 12:35:12 +0100

    Implemented initial gate fragment

---
+++ app/src/main/java/com/example/iotproject/GateFragment.kt
@@ -0,0 +1,30 @@
+package com.example.iotproject
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class GateFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_gate, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ImageView>(R.id.gate_image).setOnClickListener(){
+            val showDescription = it.contentDescription.toString()
+            Toast.makeText(context, showDescription, Toast.LENGTH_SHORT).show()
+        }
+
+        view.findViewById<Button>(R.id.button).setOnClickListener() {
+            Toast.makeText(context, "Button pressed", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/example/iotproject/MainActivity.kt
+++ app/src/main/java/com/example/iotproject/MainActivity.kt
@@ -2,20 +2,35 @@ package com.example.iotproject
 
 import android.annotation.TargetApi
 import android.app.Activity
+import android.content.Context
 import android.graphics.drawable.Drawable
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         //val toolbar: Toolbar = findViewById(R.id.toolbar)
         // toolbar.setTitleTextColor(Color.white)
+        val profilePicture = findViewById<ImageButton>(R.id.profile_button)
+        profilePicture.setOnClickListener(){
+            Toast.makeText(this, "Profile image", Toast.LENGTH_SHORT).show()
+            //TODO: Put profile settings (maybe other View)
 
-        setContentView(R.layout.activity_main)
+        }
     }
+
+
+
 }
