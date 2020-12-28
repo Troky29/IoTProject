2e1e6fdba30a96eecf12191f2ecc756b0b9bdb73
Troky <lorenzorosinitroky@live.it>
2020-12-28 17:19:20 +0100

    Added bottom navigation bar interactivity

--- app/src/main/java/com/example/iotproject/MainActivity.kt
+++ app/src/main/java/com/example/iotproject/MainActivity.kt
@@ -14,6 +14,7 @@ import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +25,35 @@ class MainActivity : AppCompatActivity() {
         //val toolbar: Toolbar = findViewById(R.id.toolbar)
         // toolbar.setTitleTextColor(Color.white)
         val profilePicture = findViewById<ImageButton>(R.id.profile_button)
-        profilePicture.setOnClickListener(){
+        profilePicture.setOnClickListener() {
             Toast.makeText(this, "Profile image", Toast.LENGTH_SHORT).show()
             //TODO: Put profile settings (maybe other View)
-
         }
+
+        val navigation = findViewById<BottomNavigationView>(R.id.navigation_bar)
+        val navigationListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.gate_page -> {
+                        //TODO: go to selected page
+                        Toast.makeText(this, "Gate page", Toast.LENGTH_SHORT)
+                        true
+                    }
+                    R.id.activity_page -> {
+                        //TODO: go to activity page
+                        Toast.makeText(this, "Activity page", Toast.LENGTH_SHORT)
+                        true
+                    }
+                    R.id.more_page -> {
+                        //TODO: go to more page
+                        Toast.makeText(this, "More page", Toast.LENGTH_SHORT)
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+        navigation.setOnNavigationItemSelectedListener(navigationListener)
+
     }
-
-
 
 }
