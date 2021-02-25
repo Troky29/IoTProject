9a6edc380b1e66a659dc88ec6112f5accef71600
Troky <Lorenzorosinitroky@live.it>
2021-02-25 10:57:20 +0100

    Added multiple gates scrolling with snap

--- app/src/main/java/com/example/iotproject/GateCardAdapter.kt
+++ app/src/main/java/com/example/iotproject/GateCardAdapter.kt
@@ -3,11 +3,14 @@ package com.example.iotproject
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class GateCardAdapter(private val exampleList: List<GateCardItem>) : RecyclerView.Adapter<GateCardAdapter.CardViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GateCardAdapter.CardViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.gate_card, parent, false)
         return CardViewHolder(itemView)
     }
@@ -15,14 +18,23 @@ class GateCardAdapter(private val exampleList: List<GateCardItem>) : RecyclerVie
     override fun onBindViewHolder(holder: GateCardAdapter.CardViewHolder, position: Int) {
         val currentItem = exampleList[position]
 
-        //TODO: Customize the single gate entry
+        holder.gateImage.setImageResource(currentItem.imageResource)
+        holder.gateName.text = currentItem.name
+        holder.gateLocation.text = currentItem.location
+        holder.gateState.text = currentItem.state
+        holder.gateId.text = currentItem.id
     }
 
     override fun getItemCount(): Int = exampleList.size
 
     class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        //TODO: Customazi the gate values
+        //TODO: Customaze the gate values
+        val gateImage: ImageView = itemView.findViewById(R.id.gateImageView)
+        val gateName: TextView = itemView.findViewById(R.id.gateNameTextView)
+        val gateLocation: TextView = itemView.findViewById(R.id.gateLocationTextView)
+        val gateState: TextView = itemView.findViewById(R.id.gateStateTextView)
+        val gateId: TextView = itemView.findViewById(R.id.gateIdTextView)
     }
 }
 
-data class GateCardItem (val imageResource: Int, val id: String, val location: String, val state: String)
+data class GateCardItem (val imageResource: Int, val name: String, val location: String, val state: String, val id: String)
--- app/src/main/java/com/example/iotproject/GateFragment.kt
+++ app/src/main/java/com/example/iotproject/GateFragment.kt
@@ -8,7 +8,10 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
 
 class GateFragment: Fragment() {
 
@@ -19,6 +22,7 @@ class GateFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /*
         view.findViewById<ImageView>(R.id.gate_image).setOnClickListener(){
             val showDescription = it.contentDescription.toString()
             Toast.makeText(context, showDescription, Toast.LENGTH_SHORT).show()
@@ -29,7 +33,18 @@ class GateFragment: Fragment() {
             //TODO: Move activity to login, for changing user
         }
 
+         */
+
+        val item = GateCardItem(R.drawable.hqdefault, "Name", "Location", "State", "Id")
+        val exampleList = ArrayList<GateCardItem>()
+        for (i in 1..3) exampleList += item
+
         val recyclerView: RecyclerView = view.findViewById(R.id.gateRecycleView)
-        //recyclerView.adapter = CardAdapter
+        recyclerView.adapter = GateCardAdapter(exampleList)
+        recyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.setHasFixedSize(true)
+
+        val snapHelper: SnapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerView)
     }
 }
